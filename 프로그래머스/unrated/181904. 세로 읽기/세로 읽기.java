7eec1c7939510b246b2868f9574f1844873b8a32
drnomader <71203649+drnomader@users.noreply.github.com>
2023-11-29 23:49:39 +0900

    [level unrated] Title: 세로 읽기, Time: 11.62 ms, Memory: 77.1 MB -BaekjoonHub

--- 프로그래머스/unrated/181904. 세로 읽기/세로 읽기.java
+++ 프로그래머스/unrated/181904. 세로 읽기/세로 읽기.java
@@ -1,16 +1,8 @@
-import java.util.*;
-
 class Solution {
-    public String solution(String my_string, int m, int c) {
-        
-        List<String> strList = new ArrayList<>();
-        for (int i = 0; i < my_string.length(); i += m) {
-            String subStr = my_string.substring(i, i + m);
-            strList.add(subStr);
-        }
+    public String solution(String my_string, int m, int c) {   
         String answer = "";
-        for (String str : strList) {
-            answer += str.substring(c-1, c);
+        for (int i = c - 1; i < my_string.length(); i += m) {
+            answer += my_string.charAt(i);
         }
         return answer;
     }
