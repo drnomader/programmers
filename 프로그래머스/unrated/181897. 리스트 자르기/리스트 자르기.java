ef5f6b210f7aa8a132e23573bb228ffb1312a50a
drnomader <71203649+drnomader@users.noreply.github.com>
2023-12-05 00:09:34 +0900

    [level unrated] Title: 리스트 자르기, Time: 0.02 ms, Memory: 78 MB -BaekjoonHub

--- 프로그래머스/unrated/181897. 리스트 자르기/리스트 자르기.java
+++ 프로그래머스/unrated/181897. 리스트 자르기/리스트 자르기.java
@@ -1,30 +1,14 @@
-import java.util.*;
-
 class Solution {
     public int[] solution(int n, int[] slicer, int[] num_list) {
-        List<Integer> list = new ArrayList<>();
-        int a = slicer[0];
-        int b = slicer[1];
-        int c = slicer[2];
-        if(n == 1) {
-            for(int i =0; i<b+1; i++) {
-                list.add(num_list[i]);
-            }
-        }else if(n == 2) {
-            for(int i = a; i<num_list.length; i++) {
-                list.add(num_list[i]);
-            }
-        }else if(n ==3) {
-            for(int i = a; i<b+1; i++) {
-                list.add(num_list[i]);
-            }
-        }else if(n == 4) {
-            for(int i = a; i<b+1; i+=c) {
-                list.add(num_list[i]);
-
-            }
+        
+        int start = n == 1 ? 0 : slicer[0];
+        int end = n == 2 ? num_list.length - 1 : slicer[1];
+        int step = n == 4 ? slicer[2] : 1;
+        int[] answer = new int[(end - start + step) / step];
+        
+        for (int i = start, j = 0; i <= end; i += step) {
+            answer[j++] = num_list[i];
         }
-        int[] answer = list.stream().mapToInt(x -> x).toArray();
         return answer;
     }
 }
