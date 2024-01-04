a790482dd614856e970f7596b08366082030e02b
drnomader <71203649+drnomader@users.noreply.github.com>
2024-01-04 22:24:30 +0900

    [level unrated] Title: n 번째 원소까지, Time: 0.01 ms, Memory: 75.1 MB -BaekjoonHub

--- 프로그래머스/unrated/181889. n 번째 원소까지/n 번째 원소까지.java
+++ 프로그래머스/unrated/181889. n 번째 원소까지/n 번째 원소까지.java
@@ -1,11 +1,7 @@
-import java.util.*;
+import java.util.Arrays;
 
 class Solution {
     public int[] solution(int[] num_list, int n) {
-        int[] answer = {};
-        for(int i = 0; i < n; i++) {
-            return Arrays.copyOfRange(num_list, i, n);
-        }
-        return answer;
+        return Arrays.copyOfRange(num_list, 0, n);
     }
 }
