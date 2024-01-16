588d203c1dc421fd3e790c20b7e10616029dca8b
drnomader <71203649+drnomader@users.noreply.github.com>
2024-01-16 21:57:05 +0900

    [level 0] Title: 1로 만들기, Time: 1.18 ms, Memory: 76.8 MB -BaekjoonHub

--- 프로그래머스/0/181880. 1로 만들기/1로 만들기.java
+++ 프로그래머스/0/181880. 1로 만들기/1로 만들기.java
@@ -1,12 +1,9 @@
+import java.util.Arrays;
+
 class Solution {
     public int solution(int[] num_list) {
-        int answer = 0;
-        for(int i = 0; i < num_list.length; i++) {
-            while(num_list[i] != 1) {
-                num_list[i] /= 2;
-                answer++;
-            }
-        }
-        return answer;
+        return Arrays.stream(num_list)
+            .map(i -> Integer.toBinaryString(i).length() - 1)
+            .sum();
     }
 }
