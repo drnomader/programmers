6d61c65a176f1cfc78871d71f2e27bc4b3a30ffa
drnomader <71203649+drnomader@users.noreply.github.com>
2024-01-23 23:34:58 +0900

    [level 0] Title: A 강조하기, Time: 0.06 ms, Memory: 73.6 MB -BaekjoonHub

--- 프로그래머스/0/181874. A 강조하기/A 강조하기.java
+++ 프로그래머스/0/181874. A 강조하기/A 강조하기.java
@@ -1,17 +1,5 @@
-import java.util.*;
-
 class Solution {
     public String solution(String myString) {
-        
-        char[] myChar = myString.toCharArray();
-        
-        for(int i = 0; i < myChar.length; i++) {
-            if(myChar[i] == 'a') {
-                myChar[i] = 'A';
-            } else if(myChar[i] != 'A') {
-                myChar[i] = Character.toLowerCase(myChar[i]);
-            }
-        }
-        return new String(myChar);
+        return myString.toLowerCase().replaceAll("a", "A");
     }
 }
