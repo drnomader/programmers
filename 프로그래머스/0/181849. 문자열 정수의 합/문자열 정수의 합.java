15e19f4037957a70c4a5874204cfa662a9d92e49
drnomader <71203649+drnomader@users.noreply.github.com>
2024-03-02 14:01:01 +0900

    [level 0] Title: 문자열 정수의 합, Time: 1.31 ms, Memory: 77.8 MB -BaekjoonHub

--- 프로그래머스/0/181849. 문자열 정수의 합/문자열 정수의 합.java
+++ 프로그래머스/0/181849. 문자열 정수의 합/문자열 정수의 합.java
@@ -1,16 +1,8 @@
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-
 class Solution {
     public int solution(String num_str) {
-        int sum = 0;
-
-        Pattern pattern = Pattern.compile("\\d");
-        Matcher matcher = pattern.matcher(num_str);
-
-        while (matcher.find()) {
-            sum += Integer.parseInt(matcher.group());
-        }
-        return sum;
+        return num_str
+            .chars()
+            .map(c -> c - 48)
+            .sum();
     }
 }
