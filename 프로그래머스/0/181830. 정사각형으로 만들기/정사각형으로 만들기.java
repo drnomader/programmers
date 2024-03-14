e868db7c980fa2a4b45b60bedaa4947721b12180
drnomader <71203649+drnomader@users.noreply.github.com>
2024-03-14 23:10:22 +0900

    [level 0] Title: 정사각형으로 만들기, Time: 0.08 ms, Memory: 77.7 MB -BaekjoonHub

--- 프로그래머스/0/181830. 정사각형으로 만들기/정사각형으로 만들기.java
+++ 프로그래머스/0/181830. 정사각형으로 만들기/정사각형으로 만들기.java
@@ -1,29 +1,12 @@
-import java.util.Arrays;
-
 class Solution {
     public int[][] solution(int[][] arr) {
-        
-        int rows = arr.length;
-        int cols = 0;
-        for(int[] row : arr) {
-            cols = Math.max(cols, row.length);
+    
+        int max = Math.max(arr.length, arr[0].length);
+        int[][] array = new int[max][max];
+
+        for (int i = 0; i < arr.length; i++) {
+            System.arraycopy(arr[i], 0, array[i], 0, arr[0].length);
         }
-        
-        int[][] newArr = new int[Math.max(rows, cols)][Math.max(rows, cols)];
-        
-        for(int i = 0; i < newArr.length; i++) {
-            if(i < arr.length) {
-                for(int j = 0; j < newArr[i].length; j++) {
-                    if(j < arr[i].length) {
-                        newArr[i][j] = arr[i][j];
-                    } else {
-                        newArr[i][j] = 0;
-                    }
-                }
-            } else {
-                Arrays.fill(newArr[i], 0);
-            }
-        }
-        return newArr;
+        return array;
     }
 }
