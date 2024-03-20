179973c87866c6ad2dbbd63140242a204c916ed4
drnomader <71203649+drnomader@users.noreply.github.com>
2024-03-20 22:52:10 +0900

    [level 0] Title: 문자열 밀기, Time: 1.26 ms, Memory: 76.2 MB -BaekjoonHub

--- 프로그래머스/0/120921. 문자열 밀기/문자열 밀기.java
+++ 프로그래머스/0/120921. 문자열 밀기/문자열 밀기.java
@@ -1,16 +1,5 @@
 class Solution {
     public int solution(String A, String B) {
-        if(A.equals(B)) {
-            return 0;
-        }
-        
-        int length = A.length();
-        for(int i = 0; i < length; i++) {
-            A = A.charAt(length - 1) + A.substring(0, length - 1);
-            if(A.equals(B)) {
-                return i + 1;
-            }
-        }
-        return -1;
+        return (B + B).indexOf(A);
     }
 }
