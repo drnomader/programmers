ce63120649773b730dd7bc355647b1b664e9ea99
drnomader <71203649+drnomader@users.noreply.github.com>
2024-03-25 23:30:38 +0900

    [level 0] Title: 7의 개수, Time: 0.03 ms, Memory: 68.8 MB -BaekjoonHub

---
+++ 프로그래머스/0/120912. 7의 개수/7의 개수.java
@@ -0,0 +1,16 @@
+import java.util.Arrays;
+
+class Solution {
+    public int solution(int[] array) {
+        int count = 0;
+        for (int num : array) {
+            while (num > 0) {
+                if (num % 10 == 7) {
+                    count++;
+                }
+                num /= 10;
+            }
+        }
+        return count;
+    }
+}
