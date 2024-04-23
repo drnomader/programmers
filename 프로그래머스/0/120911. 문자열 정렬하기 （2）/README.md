e2fcadc80ad6031093794d3aa7c4760ed7858f2d
drnomader <71203649+drnomader@users.noreply.github.com>
2024-04-24 07:21:10 +0900

    [level 0] Title: 문자열 정렬하기 (2), Time: 2.23 ms, Memory: 81.3 MB -BaekjoonHub

--- 프로그래머스/0/120911. 문자열 정렬하기 （2）/README.md
+++ 프로그래머스/0/120911. 문자열 정렬하기 （2）/README.md
@@ -1,10 +1,10 @@
 # [level 0] 문자열 정렬하기 (2) - 120911 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120911?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120911) 
 
 ### 성능 요약
 
-메모리: 81.9 MB, 시간: 0.97 ms
+메모리: 81.3 MB, 시간: 2.23 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 04월 24일 07:19:05
+2024년 04월 24일 07:21:09
 
 ### 문제 설명
 
--- 프로그래머스/0/120911. 문자열 정렬하기 （2）/문자열 정렬하기 （2）.java
+++ 프로그래머스/0/120911. 문자열 정렬하기 （2）/문자열 정렬하기 （2）.java
@@ -2,8 +2,8 @@ import java.util.Arrays;
 
 class Solution {
     public String solution(String my_string) {
-        String my_string_lower_case = my_string.toLowerCase();
-        char[] char_array = my_string_lower_case.toCharArray();
+        String lower_case = my_string.toLowerCase();
+        char[] char_array = lower_case.toCharArray();
         Arrays.sort(char_array);
         return new String(char_array);
     }
