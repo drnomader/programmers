6fc99d7fe329e29ad1722b62b8408dd37bdd6acf
drnomader <71203649+drnomader@users.noreply.github.com>
2024-06-10 23:28:26 +0900

    [level 0] Title: OX퀴즈, Time: 0.08 ms, Memory: 73.5 MB -BaekjoonHub

--- 프로그래머스/0/120907. OX퀴즈/OX퀴즈.java
+++ 프로그래머스/0/120907. OX퀴즈/OX퀴즈.java
@@ -1,23 +1,10 @@
 class Solution {
     public String[] solution(String[] quiz) {
-        
-        String[][] formula = new String[quiz.length][5];
-        String[] answer = new String[quiz.length];
-        
         for(int i = 0; i < quiz.length; i++) {
-            formula[i] = quiz[i].split(" ");
-        
-            int num1 = Integer.parseInt(formula[i][0]);
-            int num2 = Integer.parseInt(formula[i][2]);
-            int result_num = Integer.parseInt(formula[i][4]);
-            String operator = formula[i][1];
-        
-            if(operator.equals("+")) {
-                answer[i] = (num1 + num2) == result_num ? "O" : "X";
-            } else if(operator.equals("-")) {
-                answer[i] = (num1 - num2) == result_num ? "O" : "X";
-            }
+            String[] formula = quiz[i].split(" ");
+            int result = Integer.parseInt(formula[0]) + ( Integer.parseInt(formula[2]) * (formula[1].equals("+") ? 1 : -1) );
+            quiz[i] = ( result == Integer.parseInt(formula[4]) ) ? "O" : "X";
         }
-        return answer;
+        return quiz;
     }
 }
