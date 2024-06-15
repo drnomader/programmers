c6678f95d463ab6c71311e3ce9272513db54c869
drnomader <71203649+drnomader@users.noreply.github.com>
2024-06-15 16:12:13 +0900

    [level 0] Title: 숫자 찾기, Time: 8.32 ms, Memory: 76.5 MB -BaekjoonHub

--- 프로그래머스/0/120904. 숫자 찾기/숫자 찾기.java
+++ 프로그래머스/0/120904. 숫자 찾기/숫자 찾기.java
@@ -1,8 +1,5 @@
-import java.util.Arrays;
-
 class Solution {
     public int solution(int num, int k) {
-        int index = Integer.toString(num).indexOf(Integer.toString(k));
-        return (index == -1) ? index : index + 1;
+        return ("-" + num).indexOf(String.valueOf(k));
     }
 }
