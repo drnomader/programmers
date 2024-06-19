6fdc1a3b77aabc2186039b3ecb65c825b8590c5c
drnomader <71203649+drnomader@users.noreply.github.com>
2024-06-20 07:25:32 +0900

    [level 0] Title: 한 번만 등장한 문자, Time: 0.03 ms, Memory: 76.3 MB -BaekjoonHub

--- 프로그래머스/0/120896. 한 번만 등장한 문자/한 번만 등장한 문자.java
+++ 프로그래머스/0/120896. 한 번만 등장한 문자/한 번만 등장한 문자.java
@@ -1,15 +1,16 @@
-import java.util.*;
-import java.util.stream.Collectors;
-
 class Solution {
     public String solution(String s) {
-        return s.chars()
-            .mapToObj(c -> (char) c)
-            .collect(Collectors.groupingBy(c -> c, LinkedHashMap::new, Collectors.counting()))
-            .entrySet().stream()
-            .filter(e -> e.getValue() == 1L)
-            .map(e -> String.valueOf(e.getKey()))
-            .sorted()
-            .collect(Collectors.joining());
+        int[] alpha = new int[26];
+        for(char c : s.toCharArray()){
+            alpha[c - 'a']++;
+        }
+
+        StringBuilder answer = new StringBuilder();
+        for(int i = 0; i < 26; i++){
+            if(alpha[i] == 1){
+                answer.append((char)(i + 'a'));
+            }
+        }
+        return answer.toString(); 
     }
 }
