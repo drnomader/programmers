cb1e7361e299cb2807632bbf477cf7145f8e3816
drnomader <71203649+drnomader@users.noreply.github.com>
2024-07-03 06:12:37 +0900

    [level 0] Title: 암호 해독, Time: 1.24 ms, Memory: 73.2 MB -BaekjoonHub

--- 프로그래머스/0/120892. 암호 해독/암호 해독.java
+++ 프로그래머스/0/120892. 암호 해독/암호 해독.java
@@ -1,12 +1,11 @@
-import java.util.stream.Collectors;
-import java.util.stream.IntStream;
-
 class Solution {
     public String solution(String cipher, int code) {
-        return IntStream.range(0, cipher.length())
-            .filter(i -> (i + 1) % code == 0)
-            .mapToObj(cipher::charAt)
-            .map(String::valueOf)
-            .collect(Collectors.joining());
+        String answer = "";
+
+        for (int i = code; i <= cipher.length(); i = i + code) {
+            answer += cipher.substring(i - 1, i);
+        }
+
+        return answer;
     }
 }
