ed8cce6a4fb1511dd57a1548eae77697ba2f0b7e
drnomader <71203649+drnomader@users.noreply.github.com>
2024-07-17 22:36:28 +0900

    [level 0] Title: A로 B 만들기, Time: 1.16 ms, Memory: 68.9 MB -BaekjoonHub

---
+++ 프로그래머스/0/120886. A로 B 만들기/A로 B 만들기.java
@@ -0,0 +1,13 @@
+import java.util.Arrays;
+
+class Solution {
+    public int solution(String before, String after) {
+        char[] beforeChars = before.toCharArray();
+        char[] afterChars = after.toCharArray();
+        
+        Arrays.sort(beforeChars);
+        Arrays.sort(afterChars);
+        
+        return Arrays.equals(beforeChars, afterChars) ? 1 : 0;
+    }
+}
