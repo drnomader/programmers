9717c9c8f32d588d689fa453c0944a90ad517bfc
Dr. Nomader <71203649+drnomader@users.noreply.github.com>
2025-02-11 15:44:12 +0900

    [level 0] Title: 특이한 정렬, Time: 17.57 ms, Memory: 79.7 MB -BaekjoonHub

--- 프로그래머스/0/120880. 특이한 정렬/특이한 정렬.java
+++ 프로그래머스/0/120880. 특이한 정렬/특이한 정렬.java
@@ -2,17 +2,18 @@ import java.util.*;
 
 class Solution {
     public int[] solution(int[] numlist, int n) {
-        Arrays.sort(numlist);
-        
-        for (int i = 0; i < numlist.length; i++) {
-            for (int j = 0; j < numlist.length; j++) {
-                if (Math.abs(n - numlist[i]) <= Math.abs(n - numlist[j])) {
-                    int tmp = numlist[i];
-                    numlist[i] = numlist[j];
-                    numlist[j] = tmp;
+        return Arrays.stream(numlist)
+            .mapToObj(a -> (Integer) a)
+            .sorted((a, b) -> {
+                int i = Math.abs(n - a);
+                int j = Math.abs(n - b);
+                if (i == j) {
+                    return b - a;
+                } else {
+                    return i - j;
                 }
-            }
-        }
-        return numlist;
+            })
+            .mapToInt(a -> a)
+            .toArray();
     }
 }
