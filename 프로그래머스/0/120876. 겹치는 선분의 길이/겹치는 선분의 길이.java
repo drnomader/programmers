07a9ad936e9eaaa073ad50a73035245898b48449
Dr. Nomader <71203649+drnomader@users.noreply.github.com>
2025-03-11 23:43:30 +0900

    [level 0] Title: 겹치는 선분의 길이, Time: 0.09 ms, Memory: 93 MB -BaekjoonHub

--- 프로그래머스/0/120876. 겹치는 선분의 길이/겹치는 선분의 길이.java
+++ 프로그래머스/0/120876. 겹치는 선분의 길이/겹치는 선분의 길이.java
@@ -1,19 +1,21 @@
 class Solution {
     public int solution(int[][] lines) {
-        int[] arr = new int[200];
         int answer = 0;
-        
-        for (int i = 0; i < lines.length; i++) {
-            for (int j = lines[i][0] + 100; j < lines[i][1] + 100; j++) {
-                arr[j]++;
+
+        for (int i = -100; i < 100; i++) {
+            int count = 0;
+
+            // 모든 선분을 확인하여 겹치는 개수 카운트
+            for (int[] line : lines) {
+                if (line[0] <= i && line[1] > i) {
+                    count++;
+                }
             }
+
+            // 두 개 이상 겹치는 경우 answer 증가
+            if (count > 1) answer++;
         }
-        
-        for (int i = 0; i < 200; i++) {
-            if (arr[i] > 1) {
-                answer++;
-            }
-        }
+
         return answer;
     }
 }
