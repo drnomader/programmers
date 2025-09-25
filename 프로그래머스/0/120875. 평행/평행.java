0b7e638596062078e538bdba6eb6564fee0d2b25
Dr. Nomader <71203649+drnomader@users.noreply.github.com>
2025-09-25 22:34:49 +0900

    [level 0] Title: 평행, Time: 0.04 ms, Memory: 73.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/120875. 평행/평행.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int solution(int[][] dots) {
+        // 점들을 꺼내서 변수에 저장
+        int x1 = dots[0][0], y1 = dots[0][1];
+        int x2 = dots[1][0], y2 = dots[1][1];
+        int x3 = dots[2][0], y3 = dots[2][1];
+        int x4 = dots[3][0], y4 = dots[3][1];
+        int answer = 0;
+
+        // 조합 1: (0,1) vs (2,3)
+        double slope1 = (double) (y2 - y1) / (x2 - x1);
+        double slope2 = (double) (y4 - y3) / (x4 - x3);
+        if (slope1 == slope2) answer = 1;
+
+        // 조합 2: (0,2) vs (1,3)
+        slope1 = (double) (y3 - y1) / (x3 - x1);
+        slope2 = (double) (y2 - y4) / (x2 - x4);
+        if (slope1 == slope2) answer = 1;
+
+        // 조합 3: (0,3) vs (1,2)
+        slope1 = (double) (y4 - y1) / (x4 - x1);
+        slope2 = (double) (y2 - y3) / (x2 - x3);
+        if (slope1 == slope2) answer = 1;
+
+        return answer;
+    }
+}
