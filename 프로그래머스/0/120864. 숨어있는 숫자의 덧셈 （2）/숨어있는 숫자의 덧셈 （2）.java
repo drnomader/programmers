6497192d3877190c25e388b223e7331efdccfeca
Dr. Nomader <71203649+drnomader@users.noreply.github.com>
2025-10-01 22:10:27 +0900

    [level 0] Title: 숨어있는 숫자의 덧셈 (2), Time: 0.16 ms, Memory: 78.9 MB -BaekjoonHub

---
+++ 프로그래머스/0/120864. 숨어있는 숫자의 덧셈 （2）/숨어있는 숫자의 덧셈 （2）.java
@@ -0,0 +1,15 @@
+import java.util.regex.*;
+
+class Solution {
+    public int solution(String my_string) {
+        int sum = 0;
+        
+        Matcher matcher = Pattern.compile("\\d+").matcher(my_string);
+        
+        while (matcher.find()) {
+            sum += Integer.parseInt(matcher.group());
+        }
+        
+        return sum;
+    }
+}
