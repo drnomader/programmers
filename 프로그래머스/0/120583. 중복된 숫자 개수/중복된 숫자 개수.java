1fe2e9083d493f503127a0360de3a3424104bd78
Dr. Nomader <71203649+drnomader@users.noreply.github.com>
2025-11-06 20:11:57 +0900

    [level 0] Title: 중복된 숫자 개수, Time: 0.02 ms, Memory: 87.9 MB -BaekjoonHub

---
+++ 프로그래머스/0/120583. 중복된 숫자 개수/중복된 숫자 개수.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] array, int n) {
+        int count = 0;
+        
+        for (int num : array) {
+            if (num == n) {
+                count++;
+            }
+        }
+        
+        return count;
+    }
+}
