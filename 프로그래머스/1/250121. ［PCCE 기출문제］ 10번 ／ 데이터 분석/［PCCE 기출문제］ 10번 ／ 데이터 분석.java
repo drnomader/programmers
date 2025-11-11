779a9f2b0dafbde25a643e0ddcf85aa23fec1b0c
Dr. Nomader <71203649+drnomader@users.noreply.github.com>
2025-11-11 22:32:44 +0900

    [level 1] Title: [PCCE 기출문제] 10번 / 데이터 분석, Time: 6.87 ms, Memory: 88.1 MB -BaekjoonHub

--- 프로그래머스/1/250121. ［PCCE 기출문제］ 10번 ／ 데이터 분석/［PCCE 기출문제］ 10번 ／ 데이터 분석.java
+++ 프로그래머스/1/250121. ［PCCE 기출문제］ 10번 ／ 데이터 분석/［PCCE 기출문제］ 10번 ／ 데이터 분석.java
@@ -1,29 +1,19 @@
 import java.util.*;
+import java.util.stream.*;
 
 class Solution {
     public int[][] solution(int[][] data, String ext, int val_ext, String sort_by) {
-        Map<String, Integer> colIndex = new HashMap<>();
-        colIndex.put("code", 0);
-        colIndex.put("date", 1);
-        colIndex.put("maximum", 2);
-        colIndex.put("remain", 3);
         
-        int extIndex = colIndex.get(ext);
-        int sortByIndex = colIndex.get(sort_by);
+        String[] columns = {"code", "date", "maximum", "remain"};
+        List<String> columnList = Arrays.asList(columns);
         
-        List<int[]> filteredList = new ArrayList<>();
-        for (int[] row : data) {
-            if (row[extIndex] < val_ext) {
-                filteredList.add(row);
-            }
-        }
+        int filterIndex = columnList.indexOf(ext);
+        int sortIndex = columnList.indexOf(sort_by);
         
-        filteredList.sort((a, b) -> a[sortByIndex] - b[sortByIndex]);
-        
-        int[][] answer = new int[filteredList.size()][4];
-        for (int i = 0; i < filteredList.size(); i++) {
-            answer[i] = filteredList.get(i);
-        }
+        int[][] answer = Arrays.stream(data)
+            .filter(row -> row[filterIndex] < val_ext)
+            .sorted((row1, row2) -> Integer.compare(row1[sortIndex], row2[sortIndex]))
+            .toArray(int[][]::new);
         
         return answer;
     }
