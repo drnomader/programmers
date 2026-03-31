3fd5e52140a3052fed22cb2419ef481c073e7f34
Dr. Nomader <71203649+drnomader@users.noreply.github.com>
2026-03-31 21:36:06 +0900

    [level 1] Title: 푸드 파이트 대회, Time: 1.02 ms, Memory: 94.8 MB -BaekjoonHub

---
+++ 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.java
@@ -0,0 +1,21 @@
+class Solution {
+    public String solution(int[] food) {
+        StringBuilder left = new StringBuilder();
+        
+        for (int i = 1; i < food.length; i++) {
+            int count = food[i] / 2;
+            
+            for (int j = 0; j < count; j++) {
+                left.append(i);
+            }
+        }
+        
+        StringBuilder right = new StringBuilder(left).reverse();
+        
+        left.append("0");
+        
+        left.append(right);
+        
+        return left.toString();
+    }
+}
