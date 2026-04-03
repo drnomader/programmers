4137a34bb8d9d9b532288c06b92535a884c7aa60
Dr. Nomader <71203649+drnomader@users.noreply.github.com>
2026-04-03 22:09:47 +0900

    [level 1] Title: 콜라 문제, Time: 0.07 ms, Memory: 75 MB -BaekjoonHub

---
+++ 프로그래머스/1/132267. 콜라 문제/콜라 문제.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int solution(int a, int b, int n) {
+        
+        int answer = 0;
+        
+        while (n >= a) {
+            int newCola = (n / a) * b;
+            
+            int remain = n % a;
+            
+            answer += newCola;
+            
+            n = remain + newCola;
+        }
+        
+        return answer;
+    }
+}
